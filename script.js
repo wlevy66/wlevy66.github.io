c985c417ef4821aefc46f6f8d61e0fac098128a0
William L <williamdlrq.dev@gmail.com>
2026-03-17 17:12:09 +0100

    fix: no-button outside mobile-view

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ function no() {
   const btn = document.getElementById('no');
   btn.style.position = 'fixed';
 
-  const isMobile = window.innerWidth < 768;
-  const factor = isMobile ? 0.95 : 0.85;
+  const btnW = btn.offsetWidth;
+  const btnH = btn.offsetHeight;
 
-  const windowH = window.innerHeight * factor;
-  const windowW = window.innerWidth * factor;
+  const isMobile = window.innerWidth < 768;
+  const factor = isMobile ? 0.85 : 0.85;
+
+  const windowH = (window.innerHeight * factor) - btnH;
+  const windowW = (window.innerWidth * factor) - btnW;
 
   const top = Math.floor(Math.random() * windowH);
   const left = Math.floor(Math.random() * windowW);
